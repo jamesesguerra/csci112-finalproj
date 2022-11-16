03d7e8e96a10999f629e3c68e8589ebed5dceb23
James Esguerra <james.a.esguerra@obf.ateneo.edu>
2022-11-16 23:25:29 +0800

    add patient read query

--- queries/patient_ops.py
+++ queries/patient_ops.py
@@ -1,10 +1,10 @@
-from pymongo import MongoClient
 from pprint import pprint
 
 from database import get_connection
 from utils import get_max_id
 
 
+# create
 def register_patient(name, gender, age, height, weight, contact_number):
     new_patient = {
         "patient_id": get_max_id(patients, "patient_id") + 1,
@@ -25,17 +25,32 @@ def register_patient(name, gender, age, height, weight, contact_number):
         print(f"Patient {name} was successfully registered.")
 
 
+# read
 def get_patient_info(id=None, name=None):
     if id:
-        patient = patients.find({ "patient_id": id })
-        return list(patient)
+        patient = patients.find_one({ "patient_id": id })
     else:
-        patient = patients.find({ "name": name })
-        return list(patient)
+        patient = patients.find_one({ "name": name })
+
+    return patient
+
+
+def get_patients_records(id=None, name=None):
+    if id:
+        medical_records = patients.find_one({ "patient_id": id })["medical_records"]
+    else:
+        medical_records = patients.find_one({ "name": name })["medical_records"]
+    
+    return medical_records
+
+
+# update
+
+# delete
 
 
 if __name__ == "__main__":
     db = get_connection()
     patients = db["patients"]
 
-    pprint(get_patient_info(name="Brigit Bes"))
+    pprint(get_patients_records(name="Brigit Bes"))
