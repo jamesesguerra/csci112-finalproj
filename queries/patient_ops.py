57fbee9bc815b104a66e57853f94c061435625b9
James Esguerra <james.a.esguerra@obf.ateneo.edu>
2022-11-16 15:47:41 +0800

    add query to register a new patient

--- queries/patient_ops.py
+++ queries/patient_ops.py
@@ -3,8 +3,24 @@ from pymongo import MongoClient
 from utils import get_max_id
 
 
-def register_patient():
-    pass
+def register_patient(name, gender, age, height, weight, contact_number):
+    new_patient = {
+        "patient_id": get_max_id(patients, "patient_id") + 1,
+        "name": name,
+        "gender": gender,
+        "age": age,
+        "height": height,
+        "weight": weight,
+        "contact_number": contact_number,
+        "medical_records": []
+    }
+
+    try:
+        patients.insert_one(new_patient)
+    except Exception as e:
+        print(e)
+    else:
+        print(f"Patient {name} was successfully registered.")
 
 
 if __name__ == "__main__":
@@ -18,4 +34,4 @@ if __name__ == "__main__":
 
     patients = db["patients"]
 
-    print(get_max_id(patients, "patient_id"))
+    register_patient("John dela Cruz", "Male", 45, 178, 90.5, "383-228-238")
