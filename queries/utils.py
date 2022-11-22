b2dbabe883ac44b6d718aa10b8f0c20ad548c539
James Esguerra <james.a.esguerra@obf.ateneo.edu>
2022-11-22 12:32:54 +0800

    add docstring to get max id function

--- queries/utils.py
+++ queries/utils.py
@@ -1,4 +1,9 @@
 def get_max_id(collection, field_name):
+    """
+    A function that takes in a collection in MongoDB and the field name of the 
+    id, and returns the maximum id number in that collection (just so we can mock
+    an auto increment id when we add new documents)
+    """
     result = collection.aggregate([
         {
             "$group": { "_id": 0, "max_id": { "$max": f"${field_name}" } }
