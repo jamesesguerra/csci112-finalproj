336d0f3919495993c38b7af189030ff22607cda3
James Esguerra <james.a.esguerra@obf.ateneo.edu>
2022-11-22 20:53:34 +0800

    add basic crud ops for bill entity

--- queries/bill_ops.py
+++ queries/bill_ops.py
@@ -1,11 +1,44 @@
 from pprint import pprint
+from random import randint
 
 from database import get_connection
 from utils import get_max_id
 
 
+# create
+def create_bill(amount):
+    invoice_number = ''.join(["{}".format(randint(0, 9)) for num in range(0, n)])
+
+    new_bill = {
+        "bill_id": get_max_id(bills, "bill_id") + 1,
+        "amount": amount,
+        "invoice_number": invoice_number
+    }
+
+    bills.insert_one(new_bill)
+    print(f"Bill was successfully created.")
+
+
+# read
+def get_bill_info(id):
+    bill = bills.find_one({ "bill_id": id })
+    return bill
+
+
+# update
+def update_bill_info(id, attribute, value):
+    bills.update_one({ "bill_id": id }, { "$set": { attribute: value } })
+
+    print("Bill successfully updated.")
+
+
+# delete
+def remove_bill(id):
+    bills.delete_one({ "bill_id": id })
+
+    print("Bill successfully deleted.")
+
+    
 if __name__ == "__main__":
     db = get_connection()
     bills = db["bills"]
-
-    
