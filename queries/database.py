997a28bb55660c2205ed2f37f9fe9a7885e68be4
James Esguerra <james.a.esguerra@obf.ateneo.edu>
2022-11-28 21:45:13 +0800

    rename client variable name

--- queries/database.py
+++ queries/database.py
@@ -3,8 +3,8 @@ from pymongo import MongoClient
 
 def get_connection():
     try:
-        cluster = MongoClient("mongodb+srv://mongo:mongo@csci112-cluster.zbudtoj.mongodb.net/HospitalAdministration?retryWrites=true&w=majority")
-        db = cluster["HospitalAdministration"]
+        client = MongoClient("mongodb+srv://mongo:mongo@csci112-cluster.zbudtoj.mongodb.net/HospitalAdministration?retryWrites=true&w=majority")
+        db = client["HospitalAdministration"]
         print("Connected to MongoDB")
         return db
     except Exception as e:
